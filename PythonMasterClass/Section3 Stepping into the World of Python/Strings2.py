3d8d3620805e4481f8badf1b7c7006bb0c6a92a6
JonathanJoo82 <jonathanjoo@gmail.com>
2024-11-09 23:29:27 -0600

    String slicing

--- PythonMasterClass/Section3 Stepping into the World of Python/Strings2.py
+++ PythonMasterClass/Section3 Stepping into the World of Python/Strings2.py
@@ -7,3 +7,29 @@ print(parrot[-3]) #l
 
 
 print(parrot[1-3]) #evaluate the expression for this and use the value as the index point
+
+
+#slicing or substring
+print(parrot[0:6])
+print(parrot[:6])
+print(parrot[10:])
+print(parrot[:])
+
+print(parrot[-4:-2]) #Bl
+print(parrot[-4:12]) #Bl
+
+
+
+#using a step in slice.
+
+print(parrot[0:6:2]) #Nre
+print(parrot[0:6:3]) #Nw
+
+#first line is starts at index position of 0.
+#extends up to but not including the position 6.
+#Step through the sequence in steps of 2.
+
+print(parrot[0:7:2]) #Nrei
+
+#slicing backwards
+print(parrot[::-1])
