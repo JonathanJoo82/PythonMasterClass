7376af2f728ca26bb69929ee4c8ed9b2d9998a6e
JonathanJoo82 <jonathanjoo@gmail.com>
2024-11-11 18:34:04 -0600

    asdf

---
+++ PythonMasterClass/Section3 Stepping into the World of Python/StringOperators.py
@@ -0,0 +1,20 @@
+#Python has 5 sequence types built in.
+#string type
+#list
+#tuple
+#range
+#bytes anbd bytearry
+
+str1 = "Kishan "
+str2 = "Huan "
+str3 = "Matt "
+
+print(str2 * 5)
+print(str1 * (1+5))
+print(str3 * 5 + "4")
+
+today = "friday"
+print("day" in today)
+print("fri" in today)
+print("test" in today)
+print("parrot" in today)
