e3ed5182e7c5522b8336f45e1d6d5e44319fdb32
JonathanJoo82 <jonathanjoo@gmail.com>
2024-11-12 10:36:08 -0600

    string replacements in print

---
+++ PythonMasterClass/Section3 Stepping into the World of Python/StringReplacementFields.py
@@ -0,0 +1,10 @@
+age = 24
+
+print("My age is " + str(age) + " years")
+
+#or
+print( "My age is {0} years".format(age))
+
+print("there are {0} days in {1}, {2}, {3}, {4},{5},{6} and {7}".format(31, "jan", "mar", "may", "jul", "aug", "oct", "dec"))
+
+#{} is the replacement field that will take the value of the index of .format
