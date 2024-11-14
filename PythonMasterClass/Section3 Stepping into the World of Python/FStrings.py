b3623ce20b01d6dec83b5197cdc0c9483c128c26
JonathanJoo82 <jonathanjoo@gmail.com>
2024-11-14 00:09:09 -0600

    Finish section 3 and start section 4

---
+++ PythonMasterClass/Section3 Stepping into the World of Python/FStrings.py
@@ -0,0 +1,20 @@
+name = "Tim"
+greeting = "Hello"
+print(greeting + name)
+
+print(greeting + " " + name)
+
+age = 24
+print(age)
+print(type(greeting))
+print(type(age))
+
+age_in_words = '2 years'
+print(name + f" is {age} years old")
+print(type(age))
+
+print(f"Pi is approximately {22/7:12.50f}")
+
+pie = 22/7
+print()
+print(f"Pi is approximately {pie:12.50f}")
---
+++ PythonMasterClass/Section3 Stepping into the World of Python/StringInterpolation.py
@@ -0,0 +1,12 @@
+#not used in Python 3 however used alot in Python 2.
+
+age = 24
+print("My age is %d years" % age)
+
+major = "years"
+minor = "months"
+print("My age is %d %s, %d %s" %(age, major, 6, minor))
+
+
+print("PI is about %f" %(22/7))
+print("PI is about %12.50f" %(22/7))
